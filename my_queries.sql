8b90e55c8d407556179a4d8b35180f33c55f78fb
Pasan Abeysekara <54240210+pasan1@users.noreply.github.com>
2023-06-28 23:48:01 +0530

    Add queries

---
+++ my_queries.sql
@@ -0,0 +1,10 @@
+CREATE DATEBASE MyGoTestDB;
+
+USE MyGoTestDB;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
